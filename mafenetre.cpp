6367b0ff50bb1f1b6f98125f8be781e00d8c896a
mfd <mfdewulf@gmail.com>
2018-09-26 11:01:22 +0200

    Création de SIGNAUX et SLOTS personnels

--- mafenetre.cpp
+++ mafenetre.cpp
@@ -3,43 +3,59 @@
  MaFenetre::MaFenetre() : QWidget()
 
 {
-    setFixedSize(300, 200);
+    setFixedSize(200, 120);
 
-    m_lcd = new QLCDNumber(this);
-    m_lcd->setSegmentStyle(QLCDNumber::Flat);
-    m_lcd->move(50, 20);
 
-    m_progress = new QProgressBar(this);
-    m_progress->setGeometry(10,120,150,20);
+    m_sliderH = new QSlider(Qt::Horizontal, this);
+    m_sliderH->setRange(200,600);
+    m_sliderH->setGeometry(10,60,150,20);
 
-    m_slider = new QSlider(Qt::Horizontal, this);
-    m_slider->setGeometry(10,60,150,20);
+    m_sliderV = new QSlider(Qt::Vertical, this);
+    m_sliderV->setRange(120,500);
+    m_sliderV->setGeometry(170,10,20,100);
 
-    QObject::connect(m_slider, SIGNAL(valueChanged(int)), m_lcd, SLOT(display(int))) ;
-
-    QObject::connect(m_slider, SIGNAL(valueChanged(int)), m_progress, SLOT(setValue(int))) ;
-}
+    QObject::connect(m_sliderH, SIGNAL(valueChanged(int)), this, SLOT(changerLargeur(int))) ;
+    QObject::connect(m_sliderV, SIGNAL(valueChanged(int)), this, SLOT(changerHauteur(int))) ;
+    QObject::connect(this, SIGNAL(agrandissementMax()), qApp, SLOT(quit()));
+ }
 
  MaFenetre::MaFenetre(int p_largeur, int p_hauteur) : QWidget()
  {
      setFixedSize(p_largeur, p_hauteur);
 
-     m_lcd = new QLCDNumber(this);
-     m_lcd->setSegmentStyle(QLCDNumber::Flat);
-     m_lcd->move(50, 20);
+     m_sliderH = new QSlider(Qt::Horizontal, this);
+     m_sliderH->setRange(200,600);
+     m_sliderH->setGeometry(10,60,150,20);
 
-     m_slider = new QSlider(Qt::Horizontal, this);
-     m_slider->setGeometry(10,60,150,20);
+     m_sliderV = new QSlider(Qt::Vertical, this);
+     m_sliderV->setRange(120,500);
+     m_sliderV->setGeometry(170,10,20,100);
 
-     m_progress = new QProgressBar(this);
-     m_progress->setGeometry(10,120,150,20);
+     QObject::connect(m_sliderH, SIGNAL(valueChanged(int)), this, SLOT(changerLargeur(int))) ;
+     QObject::connect(m_sliderV, SIGNAL(valueChanged(int)), this, SLOT(changerHauteur(int))) ;
+     QObject::connect(this, SIGNAL(agrandissementMax()), qApp, SLOT(quit()));
+ }
 
-     QObject::connect(m_slider, SIGNAL(valueChanged(int)), m_lcd, SLOT(display(int))) ;
-     QObject::connect(m_slider, SIGNAL(valueChanged(int)), m_progress, SLOT(setValue(int))) ;
+ void MaFenetre::changerLargeur(int largeur)
+ {
+     setFixedSize(largeur, height());
+     if (largeur ==600)
+     {
+         emit agrandissementMax();
+     }
+ }
+
+ void MaFenetre::changerHauteur(int hauteur)
+ {
+     setFixedSize(width(), hauteur);
+     if (hauteur == 500)
+     {
+         emit agrandissementMax();
+     }
  }
 
 MaFenetre::~MaFenetre()
 {
-    delete m_lcd;
-    delete m_slider;
+    delete m_sliderH;
+    delete m_sliderV;
 }
--- mafenetre.h
+++ mafenetre.h
@@ -10,15 +10,23 @@
 
 class MaFenetre : public QWidget // On hérite de QWidget (IMPORTANT)
 {
+    Q_OBJECT
+
     public:
     MaFenetre();
     MaFenetre(int largeur, int hauteur);
     ~MaFenetre();
 
+    public slots:
+    void changerLargeur(int largeur);
+    void changerHauteur(int hauteur);
+
+    signals:
+    void agrandissementMax();
+
     private:
-    QLCDNumber *m_lcd;
-    QSlider *m_slider;
-    QProgressBar *m_progress;
+    QSlider *m_sliderH;
+    QSlider *m_sliderV;
 };
 
 #endif // MAFENETRE_H
